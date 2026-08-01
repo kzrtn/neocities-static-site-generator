9e6870d98c99b9497c33a9410b6e77107c3f2c51
Jolene T <kartistenn2@gmail.com>
2026-08-01 14:48:21 +0000

    can set custom index and post layout names now

--- index.js
+++ index.js
@@ -5,66 +5,51 @@ const MarkdownIt = require('markdown-it')
 const matter = require('gray-matter')
 const dayjs = require('dayjs')
 
+const config = require('./config/config.js')
+
 const createFolderPathifNotExists = require('./utils/create_folderpath_if_not_exists.js')
 const copyFilesFromPath = require('./utils/copy_files_from_folder.js')
+const { randomUUID } = require('node:crypto')
 
 const md = new MarkdownIt()
+nunjucks.configure(config.TEMPLATES_PATH, { autoescape: false })
 
-// Folder path for markdown posts
-const POST_PATH = './_posts/'
-const posts_filenames = fs.readdirSync(POST_PATH)
+// dist set up, creates folders and copies stylesheets over
+createFolderPathifNotExists(config.OUTPUT_PATH)
+createFolderPathifNotExists(config.POST_OUTPUT_PATH)
+copyFilesFromPath(config.STYLE_PATH, config.OUTPUT_PATH)
 
-// Folder path for templates
-const TEMPLATES_PATH = './_templates/'
-const templates = fs.readdirSync(TEMPLATES_PATH)
-nunjucks.configure(TEMPLATES_PATH, { autoescape: false })
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+let postsData = []
+for (const post of config.posts) {
+  const data = fs.readFileSync(`${config.POST_PATH}/${post}`, { encoding: 'utf-8', flag: 'r' })
+  const file = matter(data)
 
-// Folder path for style sheets
-const STYLE_PATH = './_styles/'
-const styles = fs.readdirSync(STYLE_PATH)
+  const content = md.render(file.content)
+  const title = file.data.title
+  const date = new Date(file.data.date)
+  const filename = dayjs(date).format('YYYY-MM-DD') + '-' + removeIllegalChar(title)
 
-// Output folder
-const OUTPUT_PATH = './dist/'
-const POST_OUTPUT_PATH = `${OUTPUT_PATH}/posts/`
-
-createFolderPathifNotExists(OUTPUT_PATH)
-createFolderPathifNotExists(POST_OUTPUT_PATH)
-copyFilesFromPath(STYLE_PATH, OUTPUT_PATH) // Copies the stylesheets to dist
-
-for (const post of posts_filenames) {
-  fs.readFile(`${POST_PATH}/${post}`, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err.message)
-      return;
-    }
-
-    const file = matter(data)
-    const content = md.render(file.content)
-    const title = file.data.title
-    const date = new Date(file.data.date)
-
-    const filename = dayjs(date).format('YYYY-MM-DD') + '-' + title.toLowerCase().replace(' ', '-')
-
-    let result = null
-    templates.forEach(template => {
-      result = nunjucks.render(template, {content: content, title: title, date: file.data.date})
-    })
-
-    fs.writeFile(`./dist/posts/${filename}.html`, result, err => {
-      if (err) {
-        console.error(err.message)
-      } else {
-        console.log('file written')
-      }
-    })    
+  postsData.push({
+    content: content,
+    title: title,
+    date: `${dayjs(date).format('YYYY-MM-DD')}`,
+    day: weekdays[date.getDay()],
+    id: randomUUID(),
+    filename: `${filename}.html`
   })
+
+  // Render the page with new data and final HTML to output
+  const output = nunjucks.render(`${config.BLOG_POST}.html`, {content: content, title: title, date: date.toDateString()})
+  fs.writeFileSync(`./dist/posts/${filename}.html`, output)
 }
 
+// Render the index page with all the posts
+const result = nunjucks.render(`${config.BLOG_INDEX}.html`, {posts: postsData.reverse()})
+fs.writeFileSync(`./dist/${config.BLOG_INDEX}.html`, result)
 
-
-/*
-const PORT = 3000
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-)
-*/
+function removeIllegalChar(s) {
+  return s.toLowerCase()
+    .replace(/[^a-z0-9\s-]/g, '')
+    .replace(/\s+/g, '-'); 
+}
