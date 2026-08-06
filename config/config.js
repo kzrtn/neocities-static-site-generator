a0bf0c26d591f08b1d72eef62d5e5ccc283def3d
Jolene T <kartistenn2@gmail.com>
2026-08-06 07:13:30 +0000

    removed unused variables, cleaned code

--- config/config.js
+++ config/config.js
@@ -1,25 +1,13 @@
 fs = require('node:fs')
 
-// Names of your templates
-const BLOG_INDEX = 'blog-index'
-const BLOG_POST = 'blog-post'
+const BLOG_INDEX = 'blog-index' // file name for blog index
+const BLOG_POST = 'blog-post' // file name for blog post
 
-// Folder path for markdown posts
-const POST_PATH = './_posts/'
-const posts = fs.readdirSync(POST_PATH)
-
-// Folder path for templates
-const TEMPLATES_PATH = './_templates/'
-const templates = fs.readdirSync(TEMPLATES_PATH)
-
-// Folder path for style sheets
-const STYLE_PATH = './_styles/'
-
-// Output folder
-const OUTPUT_PATH = './dist/'
-
-// Blog posts output folder (Default is ./dist/posts/)
-const POST_OUTPUT_PATH = `${OUTPUT_PATH}/posts/`
+const POST_PATH = './_posts/' // Folder path for markdown posts
+const TEMPLATES_PATH = './_templates/' // Folder path for templates
+const STYLE_PATH = './_styles/' // Folder path for style sheets
+const OUTPUT_PATH = './dist/' // Output folder
+const POST_OUTPUT_PATH = `${OUTPUT_PATH}/posts/` // Blog posts output folder (Default is ./dist/posts/)
 
 module.exports = {
   BLOG_INDEX,
