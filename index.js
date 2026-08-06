195ba20f69c7c1aebb04b442a87fd04df1c0b3f3
Jolene T <kartistenn2@gmail.com>
2026-08-06 05:31:51 +0000

    added serve ability

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const process = require('node:process')
 const fs = require('node:fs')
 const path = require('node:path')
+const { execSync } = require('child_process')
+const isWindows = process.platform === `win32`
+const npm = isWindows ? `npm.cmd` : `npm`
+
+const nodemon = require('nodemon')
+const bs = require('browser-sync').create()
 
 const createFolderPathifNotExists = require(path.join(__dirname, '/utils/create_folderpath_if_not_exists.js'))
 const copyFilesFromPath = require(path.join(__dirname, '/utils/copy_files_from_folder.js'))
@@ -39,4 +45,26 @@ if (process.argv[2] === 'create') {
   copyFilesFromPath(path.join(__dirname, '_styles'), path.join(process.cwd(), siteName, '_styles'))
   copyFilesFromPath(path.join(__dirname, '_templates'), path.join(process.cwd(), siteName, '_templates'))
   copyFilesFromPath(path.join(__dirname, 'config'), path.join(process.cwd(), siteName, 'config'))
+}
+
+if (process.argv[2] === 'serve') {
+  nodemon({
+    "watch": ["_posts", "_templates", "_styles", "index.js"],
+    "ext": "md,html,css,js",
+    "exec": `cd ${__dirname} && node app.js -- ${process.cwd()}`
+  })
+
+  nodemon
+    .on('start', () => console.log('Nodemon started'))
+    .on('quit', () => process.exit())
+
+  const config = require(path.join(process.cwd(), '/config/config.js'))
+  bs.init({
+    server: config.OUTPUT_PATH,
+    startPath: `/${config.BLOG_INDEX}.html`,
+    files: ["*/*"],
+    notify: false,
+    watchEvents: ["change", "add", "unlink", "addDir", "unlinkDir"]
+  })
+  bs.reload(["*.html", "*.css"])
 }
