0148136c3992a09cd32fe99c25cc3f411b2257da
Jolene T <kartistenn2@gmail.com>
2026-08-06 11:39:49 +0000

    function returns true or false based on whether folder was created

--- utils/create_folderpath_if_not_exists.js
+++ utils/create_folderpath_if_not_exists.js
@@ -3,10 +3,12 @@ fs = require('node:fs')
 
 const createFolderPathifNotExists = (path) => {
   if (!fs.existsSync(path)){
-    fs.mkdirSync(path);
+    fs.mkdirSync(path)
     // console.log(`FOLDER CREATED: '${path}'`)
+    return true
   } else {
     console.log(`FOLDER ALREADY EXISTS: '${path}'`)
+    return false
   }
 }
 
